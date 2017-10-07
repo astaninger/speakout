b532c9da66ccefe022a8abc32b83fad95eb8c0c2
astaninger <astaning@ucsd.edu>
2017-10-07 11:37:52 -0700

    Add files via upload

---
+++ flask/flask/hello.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+mongo = PyMongo(app)
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/login")
+def login():
+	return render_template('login.html')
+
+@app.route("/profile/<username>")
+def profile(username):
+	return render_template('profile.html')
+
+if __name__ == "__main__":
+	app.run(debug=True)
