62e38f2b818c9b3cc569703a72d1bc59c5adbf7b
Isaac Shanley <devamritdas@gmail.com>
2017-10-08 01:00:39 -0700

    backend

--- flask/flask/hello.py
+++ flask/flask/hello.py
@@ -17,11 +17,10 @@ def login():
         email = request.form.get('email')
         password = request.form.get('password')
         user = mongo.db.users.find_one({'email': email})
-        print(user)
         if user is not None and password == user.get('password'):
             session['logged_in'] = True
             session['user_email'] = email
-            return redirect(url_for('profile', name=user.get('name')))
+            return redirect(url_for('profile'))
         else:
             error = 'invalid username or password'
     
@@ -34,26 +33,29 @@ def logout():
     
     return redirect(url_for('login'))
 
-@app.route("/profile/<name>")
-def profile(name):
-	return render_template('profile.html', name=name)
+@app.route("/profile")
+@app.route("/profile/<email>")
+def profile(email=None):
+    if email is None:
+        email = session['user_email']
+    user = mongo.db.users.find_one({'email': email})
+    posts = [x for x in mongo.db.posts.find({'email': email})]
+    return render_template('profile.html', user=user, posts=posts)
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        print(request.form)
         user_info = {
             'email': request.form.get('email'),
             'name': request.form.get('name'),
             'password': request.form.get('password'),
             'gender': request.form.get('gender'),
             'industry': request.form.get('industry'),
-            'posts':[]
+            'race': request.form.get('race')
         }
         mongo.db.users.insert_one(user_info)
-        print(user_info)
         return redirect(url_for('login'))
-    return render_template('signup.html')
+    return render_template('register.html')
 
 @app.route("/maps")
 def maps():
@@ -63,11 +65,18 @@ def maps():
 def stats():
     return render_template('stats.html')
 
-@app.route("/songs")
-def songs():
-    mongo.db.authenticate('user', 'pass')
-    songs = [x for x in mongo.db.songs.find()]
-    return render_template('songs.html', songs=songs)
+@app.route("/post", methods=['GET', 'POST'])
+def post():
+    if request.method == 'POST':
+        post_info = {
+            'topic': request.form.get('topic'),
+            'type': request.form.get('type'),
+            'message': request.form.get('message'),
+            'email': session['user_email']
+        }
+        mongo.db.posts.insert_one(post_info)
+        return redirect(url_for('profile'))
+    return render_template('post.html')
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run()
